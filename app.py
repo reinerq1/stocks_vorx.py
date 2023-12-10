e3eff211ae0df1365f0a373d34b0492c0f7a8ad9
reinerq <reiner.quintero1@gmail.com>
2023-12-09 22:00:51 -0300

    tercer commit

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-def cargar_productos():
+from flask import Flask
+from flask import render_template, request
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'vorx'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+
+mysql.init_app(app) 
+
+if __name__ == '__main__':
+    app.run(port=3306, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""def cargar_productos():
     try:
         with open("productos.json", "r") as archivo:
             productos = json.load(archivo)
@@ -172,4 +203,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()"""
